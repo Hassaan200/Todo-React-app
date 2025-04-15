ebd262a6e6fb0828cf30504463e3b927a93364b8
Hassaan200 <hassaankhaliq45@gmail.com>
2025-04-15 15:35:33 +0500

    home page button updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <>
@@ -7,8 +7,8 @@ const Home = () => {
 <div className="text-center mt-10">
   <h1 className="text-3xl font-bold mb-4">Welcome to the To-Do App</h1>
   <p className="text-gray-600 mb-6">Stay organized, stay focused.</p>
-  <button onClick={() => navigate('/tasks')} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-    View Your Tasks
+  <button  className="bg-slate-700 text-white px-4 py-2 rounded hover:bg-slate-600 cursor-pointer">
+      <Link to="/">View Your Tasks</Link>
   </button>
 </div>
 
